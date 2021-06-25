5e5aaa26671f666f23d42aa673a37408d61c0fa8
kuriki <KyrelJerome@Users.noreply.github.com>
2021-06-25 02:47:50 -0400

    Small fix - move around comments and annotate files for clarity

--- Seshat/EnvBuilder.py
+++
@@ -1,7 +0,0 @@
-'''
-    Carries the task of building an environment for use when marking.
-    Creates the folder structures required.
-'''
-
-class EnvBuilder:
-    pass
--- Seshat/Scheduler.py
+++ Seshat/Scheduler.py
@@ -1,9 +1,13 @@
 from Seshat.Common import AssignmentModel
 import Common
 '''
-    Schedules grading from within each
-
+    Schedules grading for a specific assignment.
+    Spawns child processes to isolate risk, also de-escalating privileges when required.
 '''
 
 class Scheduler:
     def __init__(self, assignmentModel: AssignmentModel):
+        resultsLogs = []
+        for runtime in AssignmentModel:
+            # create fresh set if desired.
+            continue
