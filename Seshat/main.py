5fce576a696e55c36e4d70efb228b4a6053ac62c
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2021-07-02 22:21:57 -0400

    Changed the config file a little bit and finish load_config

--- Seshat/main.py
+++ Seshat/main.py
@@ -1,6 +1,7 @@
-import Supervisor
+# import Supervisor
 import argparse
 import yaml
+import sys
 '''
 Entrypoint for Autograder system.
 '''
@@ -11,21 +12,29 @@ def run()-> None:
 
     config = load_config(args.config_path)
 
-    sup = Supervisor(config, None)
-    Supervisor.run(folder_path, assignments)
+
+    # print(config["assignments"][1]["name"])
+
+    # sup = Supervisor(config, None)
+    # Supervisor.run(folder_path, assignments)
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        prog = "Automated system for scheduling mass marking")
-    parser.add_argument('config_path', metavar='c',
-                        type=str, nargs='?', required=True)
-    parser.add_argument('output_path', metavar='o',
-                        type=str, nargs='?', required=True)
+    parser = argparse.ArgumentParser(prog = "Automated system for scheduling mass marking")
+    parser.add_argument('config_path', type=str)
     return parser.parse_args()
 
 def load_config(path):
-    return yaml.load(path)
+    try:
+        with open(path, 'r') as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+            if data["program"] == "M.A.R.K":
+                print("- Configuration file \""+path+"\" recognized.\n- M.A.R.K Configuration Loading...")
+    except Exception as e:
+        print("- The file given is eiter not a \".yaml\" file or a not the config file that the program needs.\n- {}".format(e))
+        sys.exit(1)
+
+    return data
 
 if __name__ == "__main__":
     run()
