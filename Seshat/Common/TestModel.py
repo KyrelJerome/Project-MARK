63a9feb2dd47a6d2295cb46a67405b839289afdd
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2021-07-18 16:54:41 -0400

    Had to apply some forgotton changes.

--- Seshat/Common/TestModel.py
+++ Seshat/Common/TestModel.py
@@ -4,12 +4,14 @@ class TestModel:
 
     def __init__(self, config) -> None:
 
+        self.worth = config["worth"]
+        self.timeout = config["timeout"]
         self.prep_command = config["prep_command"]
         self.marking_command = config["marking_command"]
 
     def __str__(self) -> str:
 
         message = "This test will run the prep command: \"" + self.prep_command + "\".\n"
-        message += "Then the test will run the marking command: \"" + self.marking_command + "\".\n"
+        message += "Then the test will run the marking command: \"" + self.marking_command + "\" and will stop it after " + self.timeout + " seconds. It is worth " + self.worth + "of the final mark."
 
         return message
