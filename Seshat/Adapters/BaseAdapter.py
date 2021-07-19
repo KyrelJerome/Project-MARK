6c0cee3c77f40a985c5ee34c980530b8a84e59b2
Kristina Todic <kristinatodic28@gmail.com>
2021-07-18 20:30:37 -0400

    Added mark and worth for BaseAdapter

--- Seshat/Adapters/BaseAdapter.py
+++ Seshat/Adapters/BaseAdapter.py
@@ -1,10 +1,18 @@
 from Common import ResultsModel
-
+import re
 
 class BaseAdapter:
     # The regex stuff happens in here
     def parseOutput(self, output) -> ResultsModel:
         '''
-        Receives input from a given marker and returns an Assignment object.
+        Receives input from a given marker and returns an ResultsModel object.
         '''
-        raise NotImplementedError
+        results = ResultsModel()
+        total_grade_pattern = r"Total Grade:\s*\[(\d*)\/(\d*)\]"
+        res = re.search(total_grade_pattern, output)
+        numerator = int(res.group(1))
+        denominator = int(res.group(2))
+        results.set_question_mark(numerator)
+        results.set_question_worth(denominator)
+
+        return results
