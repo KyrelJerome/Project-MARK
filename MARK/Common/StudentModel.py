702e7d7bd2fc0b4d131a38e67f94209dd8853339
kuriki <KyrelJerome@Users.noreply.github.com>
2021-08-01 17:36:37 -0400

    Add studentModel

---
+++ MARK/Common/StudentModel.py
@@ -0,0 +1,14 @@
+# Supports a tree(Soon to enable a topographically viable graph)
+from MARK.Common.ResultsModel import ResultsModel
+
+
+class StudentModel:
+    """
+    Represents all information of a given student.
+    Attributes
+    results:List[ResultsModel]
+    utorid:str
+    mark:float
+    """
+
+
