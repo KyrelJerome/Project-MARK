f46169823625296b719b2e097f63574ec1c75bda
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2021-08-01 18:52:11 -0400

    Quick fix.

--- MARK/Util.py
+++ MARK/Util.py
@@ -5,6 +5,6 @@ class Util:
     def crunchTheNumbers(self, student_model):
         final_mark = 0.0
         for result in student_model.get_results():
-            final_mark += resmod.get_question_mark() * resmod.get_question_worth()
+            final_mark += resmod.get_question_mark() * 100 * resmod.get_question_worth()
 
         student_model.set_final_mark(final_mark)
