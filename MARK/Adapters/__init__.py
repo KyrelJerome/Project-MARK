74bb935919d4770f4708782f5f1ecdf5d5bcd9fa
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2021-09-15 19:06:08 -0400

    Ill upload this now but it needs to change later.

--- MARK/Adapters/__init__.py
+++ MARK/Adapters/__init__.py
@@ -1,6 +1,7 @@
 from .AdapterFactory import AdapterFactory
 from .AdaptersConsts import JSON_ADAPTER_ID
 from .BaseAdapter import BaseAdapter
+from .ExamAdapter import ExamAdapter
 from .Test2Adapter import Test2Adapter
 from .A2_Adapter import A2_Adapter
 from Common import ResultsModel
